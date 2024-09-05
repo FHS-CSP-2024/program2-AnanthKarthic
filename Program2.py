630d5a05a6be3a987ac11720463531b4ddc86413
Ananth Karthic <ananthkarthic2306@gmail.com>
2024-09-05 03:53:52 +0000

    done!

--- Program2.py
+++ Program2.py
@@ -63,7 +63,7 @@ print(name)
 # - another between the two names and a third one at the end of the line.
 
 name = input("What is your name? ")
-
+print("!" + name + "!" + name + "!")
 ## Problem 3 ##
 #Please write a script that: 
 # - Asks for the user's name and address. 
@@ -74,12 +74,24 @@ name = input("What is your name? ")
 #   - Street address: 91 Station Road
 #   - City and postal code: Folsom CA, 95630
 
+firstname = input("What is your first name? ")
+lastname = input("What is your last name? ")
+streetaddress = input("What is your street address? ")
+postcode = input("What is your city and postal code? ")
+print("Here is all your information together :)")
+print(firstname)
+print(lastname)
+print(streetaddress)
+print(postcode)
 ## Problem 4 ##
 #Please write a script that: 
 # - Asks for 3 words 
 # - Puts the words together with dashes and prints that out
 
-
+word1 = input("Enter your favorite word: ")
+word2 = input("Enter your second favorite word: ")
+word3 = input("Enter your third favorite word: ")
+print(word1 + "-" + word2 + "-" + word3)
 ## Problem 5 ##
 #Please write a script that: 
 # - Asks for a name and a year
@@ -91,3 +103,10 @@ name = input("What is your name? ")
 #Mary is a valiant knight, born in the year 1572. 
 #One morning Mary woke up to an awful racket: a dragon was approaching the village. 
 #Only Mary could save the village's residents.
+
+name = input("Enter a name for your very own fictional character: ")
+year = input("What year do you want him/her to be in? Enter a future year here: ")
+print(name + " is a very tech-savvy individual, born in the year " + year + ".")
+print("In a world filled with advanced technology and AI, " + name + " was known for their incredible skills.")
+print("One day in the year " + year + ", " + name + " discovered a mysterious piece of technology.")
+print("It had the power to change the world, and only " + name + " knew how to unlock its potential.")
